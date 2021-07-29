a7901fab2768514c17c5345cc29e5dea95e94698
Lakkireddypalli Nikhila <lakkireddypalli.nikhila@ibm.com>
2021-07-29 23:23:47 +0530

    Created sairam file and modified ram

--- Ram.java
+++ Ram.java
@@ -1,5 +1,7 @@
 class Ram {
 public static void main(String args[])
-
+{
+	System.out.println("Modifed the file");
+}
 
 }
---
+++ Sairam.java
@@ -0,0 +1,4 @@
+class Sairam 
+{
+	System.out.println("Chandu and nikki ,ravi and sai are friends")
+}
